e3a7fdd3a59c073c819adbdf925222cdc475cd38
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-11-15 18:02:47 +0900

    30.TaskaddInputコンポーネント

--- src/components/task/TaskCardTitle.jsx
+++ src/components/task/TaskCardTitle.jsx
@@ -18,11 +18,23 @@ const TaskCardTitle = () => {
     setIsClick(false);
   };
 
+  const handleBlur = () => {
+    setIsClick(false);
+  }
+
   return (
-    <div onClick={handleClick}>
+    <div onClick={handleClick} className="taskCardTitleInputArea">
       {isClick ? 
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} />
+        <input
+        className="taskCardTitleInput" 
+        autoFocus 
+        type="text"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={inputCardTitle}
+        maxLength="10"
+        />
       </form>
         : <h3>{inputCardTitle}</h3>}
     </div>
